eed9e327b5aefe747ddeb07600cfd549ea86c04c
gab <gabrilosgaitanidis@gmail.com>
2026-03-01 13:07:24 +0200

    commititititi

--- Backend/DB/pool.js
+++ Backend/DB/pool.js
@@ -1,7 +1,5 @@
 const { Pool } = require("pg");
 
-// connection string pulled from environment variables for flexibility and security
-// Example .env variables: DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME
 const connectionString =
   process.env.DATABASE_URL ||
   `postgresql://${process.env.DB_USER || "gabz"}:${
